56975b78d76c3aaf50321328f9d5a0bb6cda0a6e
Arpit Purwar <arpurwar@paypal.com>
2022-07-16 12:57:07 +0530

    promises with error

--- async-await.js
+++ async-await.js
@@ -17,20 +17,26 @@ const getCakes = () => {
 }
 
 const getDrink = () => {
-	return new Promise(resolve => 
+	return new Promise(reject => 
 		{
 			setTimeout(function(){
-				resolve('getDrink()')
+				reject(new Error("Whoops!"))
 			}, 4000)
 		})
 }
 
 
 async function startParty(){
+	try{
 	const fries =  await getFries()
 	const cakes = await getCakes()
 	const drink = await getDrink()
 	return {fries, cakes, drink}
+	}catch(err){
+		console.log("startParty",err)
+		return err
+	}
+
 }
 
 startParty().then(data => console.log(data) )
