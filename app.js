5a74dbb83eb908714c10e9ec28871d433319c570
Arpit Purwar <arpurwar@paypal.com>
2022-07-29 22:45:18 +0530

    creation & fetch

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const express = require('express')
-const { User, Post} = require('./models')
+const { sequelize, User, Post} = require('./models')
 const app = express()
+
+app.use(express.json())
+
+app.post('/createUser', async function(req,res){
+
+	const {name,email, age, role} = req.body;
+	try{
+		const users = await User.create({name,email,age, role});
+		res.send(users)
+	}catch(err){
+		console.log('err', err)
+		res.status(500).send({err : 'data creation failer'})
+	}
+})
+
+app.get('/getUsers', async function(req,res){
+
+	try{
+		const users = await User.findAll();
+		res.send(users)
+	}catch(err){
+		console.log('err', err)
+		res.status(500).send({err : 'fetch error'})
+	}
+})
+
 
 
 app.listen(5000, async ()=> {
 	console.log('server is running on port:5000')
-	await User.sync({force: true})
-	await Post.sync({force: true})
+	await sequelize.authenticate();
 })
