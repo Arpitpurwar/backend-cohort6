dd868f4e36bab964ed0f234ccb249e34e41b70fd
Arpit Purwar <arpurwar@paypal.com>
2022-08-06 10:48:42 +0530

    api creation

--- controller/category.js
+++ controller/category.js
@@ -33,8 +33,69 @@ async function getAllCategory(req, res){
 	}
 }
 
+async function getCategoryOnId(req,res){
+	const categoryId = req.params.id;
+
+	try{
+		const result = await Categories.findOne({
+			where : {
+				id : categoryId
+			}
+		})
+
+		res.send(result)
+	}catch(err){
+		console.log('err in getting categories based on ID', err)
+		res.status(500).send({msg : 'Internal server error'})
+	}
+}
+
+async function updateCategory(req,res){
+	const categoryId = req.params.id;
+	try{
+		const result = await Categories.findOne({
+			where : {
+				id : categoryId
+			}
+		})
+		 if(result){
+			result.name = req.body.name;
+			result.description = req.body.description;
+
+			result.save()
+
+			res.send({msg : 'category got update', 
+			updatedCategory: result})
+		 }else {
+			console.log('err in getting categories', err)
+			res.status(400).send({msg : 'category id does not exist'})
+		 }
+	}catch(err){
+		console.log('err in getting categories', err)
+		res.status(500).send({msg : 'Internal server error'})
+	}
+}
+
+async function deleteCategory(req,res){
+	const categoryId = req.params.id;
+	try{
+		const result = await Categories.destroy({
+			where :{
+				id: categoryId
+			}
+		})
+
+		res.send({msg: 'catrgory deleted', result})
+	}catch(err){
+		console.log('err in deleting categories', err)
+		res.status(500).send({msg : 'Internal server error'})
+	}
+}
 
 module.exports = {
 	createCategory,
-	getAllCategory
+	getAllCategory,
+	getCategoryOnId,
+	updateCategory,
+	deleteCategory
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const {createCategory, getAllCategory} = require('../controller/category')
+const {createCategory, getAllCategory,
+getCategoryOnId, updateCategory, deleteCategory} = require('../controller/category')
 
 const routes = express.Router()
 
@@ -8,6 +9,9 @@ routes.post('/ecomm/api/v1/categories',createCategory)
 
 routes.get('/ecomm/api/v1/categories',getAllCategory)
 
-routes.put('/ecomm/api/v1/categories/:id')
+routes.get('/ecomm/api/v1/categories/:id', getCategoryOnId)
 
+routes.put('/ecomm/api/v1/categories/:id', updateCategory)
+
+routes.delete('/ecomm/api/v1/categories/:id', deleteCategory)
 module.exports = routes
