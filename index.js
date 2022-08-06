6e773eec6db926cd496fe3804523c2284428c7fd
Arpit Purwar <arpurwar@paypal.com>
2022-08-06 09:21:56 +0530

    sync tables

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const {serverPort} = require('./configs/server.config')
+const {serverPort} = require('./config/server.config')
 const express = require('express')
-
+const { Categories, sequelize } = require('./models')
 
 const app = express()
 
 
-app.listen(serverPort, ()=> {
+app.listen(serverPort, async ()=> {
 	console.log('server is running on this port', serverPort)
+	await Categories.sync({force: true})
 })
 
