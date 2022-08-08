c047a3622c39ee13bcab226cd2f03e73664e8b2f
Arpit Purwar <arpurwar@paypal.com>
2022-08-08 22:58:24 +0530

    Update & delete

--- controller/product.js
+++ controller/product.js
@@ -44,9 +44,59 @@ async function getProductOnId(req,res){
 	}
 }
 
-async function updateProduct(req,res){}
+async function updateProduct(req,res){
+	const productData = req.body;
+	const productId = req.params.id;
+	
+	if(!(productData.name && productData.cost && productData.quantity && productData.description)){
+		res.status(400).send({msg : 'Name, Cost, Quantity & description is missing'})
+	}
 
-async function deleteProduct(req,res){}
+	try{
+		const name = productData.name;
+		const description = productData.description;
+		const cost = productData.cost;
+		const quantity = productData.quantity;
+
+		const product = await Products.findOne({
+			where:{id : productId}
+		})
+
+		if(product){
+			product.name = name;
+			product.cost = cost;
+			product.description = description;
+			product.quantity = quantity;
+
+			product.save()
+
+			res.send({msg : 'product got updated successfully'})
+		}else{
+			res.status(400).send({msg : 'product id does not exist'})
+		}
+
+
+
+	}catch(err){
+		console.log('err',err)
+		res.status(500).send({msg: 'Internal server error',err})
+	}
+
+
+}
+
+async function deleteProduct(req,res){
+	const productId = req.params.id;
+	try{
+		await Products.destroy({
+			where: {id:productId}
+		})
+
+		res.send({msg: "product delete successfully"})
+	}catch(err){
+		res.status(500).send({msg: 'Internal server error',err})
+	}
+}
 
 module.exports = {
 	createProduct,
