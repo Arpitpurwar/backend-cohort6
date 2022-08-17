57b83ccdaeb0dd18a4f867c4990af6d7747e5ed8
Arpit Purwar <arpurwar@paypal.com>
2022-08-17 22:36:08 +0530

    cart api for authenticated user

--- routes/cart.js
+++ routes/cart.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const {updateCart, getCart} = require('../controller/cart')
-
+const {verifyToken} = require('../middleware')
 const routes = express.Router()
 
 
 
-routes.put('/ecomm/api/v1/carts/:id', updateCart)
+routes.put('/ecomm/api/v1/carts/:id',[verifyToken], updateCart)
 
-routes.get('/ecomm/api/v1/carts/:id', getCart)
+routes.get('/ecomm/api/v1/carts/:id',[verifyToken], getCart)
 
 module.exports = {cartRoutes : routes}
